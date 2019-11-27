f0de39e977fda24cae0c141713b7bddf80b76c79
Dennis Morello <dennismorello@gmail.com>
2019-11-27 15:38:18 +0100

    Removed conservatives options from Google Analytics plugin

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -30,10 +30,6 @@ module.exports = {
         trackingId: process.env.GA_TRACKID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
         // Avoids sending pageview hits from custom paths
         exclude: [],
         // Delays sending pageview hits on route update (in milliseconds)
