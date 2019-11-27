e046ad87dc057027fc992b322eb5b994f079c051
Dennis Morello <dennismorello@gmail.com>
2019-11-27 14:46:51 +0100

    Added gatsby-plugin-google-analytics to docs site

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -24,6 +24,29 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GA_TRACKID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: null,
+        // Enables Google Optimize Experiment ID
+        experimentId: null,
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: null,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `React Awesome Reveal`,
