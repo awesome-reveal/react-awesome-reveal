15e5591005c09c826be5c77f6a58fcc20f3d5085
Dennis Morello <dennismorello@gmail.com>
2019-12-01 17:35:43 +0100

    Prop "title" in SEO component is no longer required (fallbacks to site name)

--- docs/src/components/seo.js
+++ docs/src/components/seo.js
@@ -102,7 +102,7 @@ SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
 }
 
 export default SEO
