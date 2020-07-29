e5424ee96e55a995545d58b4b8fab54e5e1fd937
Dennis Morello <dennismorello@gmail.com>
2020-07-29 17:33:28 +0200

    style(prettier): formatted .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: [
-    'react-app',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    "react-app",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
+      version: "detect"
+    }
+  }
 };
