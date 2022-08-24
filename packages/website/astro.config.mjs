3a305ff816325a68a6540c48d5387b124d5497d7
Dennis Morello <dennis.morello@satispay.com>
2022-08-24 21:14:23 +0200

    feat(website): added tailwind

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,4 +1,7 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+});
