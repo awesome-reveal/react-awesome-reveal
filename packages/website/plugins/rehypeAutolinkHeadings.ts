9221b7f54c539f49435edda6dc61a069d3143adc
Dennis Morello <dennis@morello.dev>
2023-01-27 17:45:07 +0100

    chore(website): update astro to version 2.0 (#164)

--- packages/website/plugins/rehypeAutolinkHeadings.ts
+++
@@ -1,6 +0,0 @@
-import type { RehypePlugins } from "astro";
-import type { Options } from "rehype-autolink-headings";
-
-const options: Options = {};
-
-export default ["rehype-autolink-headings", options] as RehypePlugins[number];
