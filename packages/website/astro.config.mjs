f8de3cb1e82f8535676840cacde13a0e2966ce89
Dennis Morello <dennismorello@gmail.com>
2024-01-05 12:26:53 +0100

    feat(website): add gtm

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 import starlight from "@astrojs/starlight";
 import vercel from "@astrojs/vercel/static";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  adapter: vercel(),
   site: "https://react-awesome-reveal.morello.dev",
   integrations: [
     starlight({
@@ -23,6 +22,24 @@ export default defineConfig({
           autogenerate: { directory: "guides" },
         },
       ],
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            type: "text/partytown",
+            src: "https://www.googletagmanager.com/gtag/js?id=GTM-PNLQX9W4",
+          },
+        },
+        {
+          tag: "script",
+          attrs: {
+            type: "text/partytown",
+          },
+          content:
+            "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','GTM-PNLQX9W4');",
+        },
+      ],
     }),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
   ],
 });
